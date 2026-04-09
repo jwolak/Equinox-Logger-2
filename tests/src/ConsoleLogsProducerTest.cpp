4db95085c67e5e3ed44018e992ed5a418078e726
Janusz <januszvdm@gmail.com>
2026-04-09 08:40:33 +0200

    Add initial unit test setup for ConsoleLogsProducer

---
+++ tests/src/ConsoleLogsProducerTest.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "ConsoleLogsProducer.h"
+
+namespace console_logs_producer_test {
+    class ConsoleLogsProducerTest : public ::testing::Test {
+       public:
+        ConsoleLogsProducerTest() {}
+    };
+}  // namespace console_logs_producer_test
