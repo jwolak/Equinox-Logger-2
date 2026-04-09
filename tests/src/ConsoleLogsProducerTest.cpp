05dffa77515bf934160a1e8ba046c67619213e34
Janusz <januszvdm@gmail.com>
2026-04-09 08:54:01 +0200

    Add ConsoleLogsProducerTest to unit tests

--- tests/src/ConsoleLogsProducerTest.cpp
+++ tests/src/ConsoleLogsProducerTest.cpp
@@ -1,10 +1,20 @@
 #include <gtest/gtest.h>
 
+#include "ColorFormatterMock.h"
 #include "ConsoleLogsProducer.h"
+#include "TimestampProducerMock.h"
 
 namespace console_logs_producer_test {
+
+    using namespace equinox;
+    using namespace testing;
+    using namespace mocks;
+
     class ConsoleLogsProducerTest : public ::testing::Test {
        public:
         ConsoleLogsProducerTest() {}
+
+        StrictMock<TimestampProducerMock>* timestamp_producer_mock;
+        StrictMock<ColorFormatterMock>* color_formatter_mock;
     };
 }  // namespace console_logs_producer_test
