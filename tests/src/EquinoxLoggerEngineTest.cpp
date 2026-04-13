b55378ddb09f0cd109df55b65905ccf04d3ab5a2
Janusz <januszvdm@gmail.com>
2026-04-13 10:16:28 +0200

    Add unit tests for EquinoxLoggerEngine to verify singleton instance and setup parameters

--- tests/src/EquinoxLoggerEngineTest.cpp
+++ tests/src/EquinoxLoggerEngineTest.cpp
@@ -23,4 +23,20 @@ namespace equinox_logger_engine_impl_test {
         StrictMock<EquinoxLoggerEngineImplMock>* equinox_logger_engine_impl_mock;
         EquinoxLoggerEngineTestable equinox_logger_engine;
     };
+
+    TEST_F(EquinoxLoggerEngineTest, Call_GetInstance_And_Returns_Same_Instance) {
+        EquinoxLoggerEngine& instance1 = EquinoxLoggerEngine::getInstance();
+        EquinoxLoggerEngine& instance2 = EquinoxLoggerEngine::getInstance();
+
+        EXPECT_EQ(&instance1, &instance2);
+    }
+
+    TEST_F(EquinoxLoggerEngineTest, Call_Setup_And_Verify_Parameters) {
+        EXPECT_CALL(*equinox_logger_engine_impl_mock, setup(level::LOG_LEVEL::info, "TestPrefix", logs_output::SINK::console, "test.log", 1024 * 1024, 5))
+            .Times(1)
+            .WillOnce(Return(true));
+
+        bool result = equinox_logger_engine.setup(level::LOG_LEVEL::info, "TestPrefix", logs_output::SINK::console, "test.log", 1024 * 1024, 5);
+        EXPECT_TRUE(result);
+    }
 }  // namespace equinox_logger_engine_impl_test
