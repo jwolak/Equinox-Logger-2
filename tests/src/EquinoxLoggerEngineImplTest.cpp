97dbcf440147a2475e9d9f9e4072e092a1027299
Janusz <januszvdm@gmail.com>
2026-04-15 09:39:23 +0200

    Refactor EquinoxLoggerEngineImplTest to initialize mocks in the constructor

--- tests/src/EquinoxLoggerEngineImplTest.cpp
+++ tests/src/EquinoxLoggerEngineImplTest.cpp
@@ -1,11 +1,15 @@
 #include <gtest/gtest.h>
 
+#include "AsyncLogQueueEngineMock.h"
 #include "EquinoxLoggerEngineImpl.h"
+#include "FileLogsProducerMock.h"
+#include "TimestampProducerMock.h"
 
 namespace equinox_logger_engine_impl_test {
 
     using namespace testing;
     using namespace equinox;
+    using namespace mocks;
 
     class EquinoxLoggerEngineImplTestable : public EquinoxLoggerEngineImpl {
        public:
@@ -16,7 +20,18 @@ namespace equinox_logger_engine_impl_test {
 
     class EquinoxLoggerEngineImplTest : public Test {
        public:
-        EquinoxLoggerEngineImplTest() {}
+        EquinoxLoggerEngineImplTest()
+            : timestamp_producer_mock{new StrictMock<TimestampProducerMock>()},
+              file_logs_producer_mock{new StrictMock<FileLogsProducerMock>()},
+              async_log_queue_engine_mock{new StrictMock<AsyncLogQueueEngineMock>()},
+              equinox_Logger_engine_impl{std::shared_ptr<ITimestampProducer>(timestamp_producer_mock),
+                                         std::shared_ptr<IFileLogsProducer>(file_logs_producer_mock),
+                                         std::unique_ptr<IAsyncLogQueueEngine>(async_log_queue_engine_mock)} {}
+
+        StrictMock<TimestampProducerMock>* timestamp_producer_mock;
+        StrictMock<FileLogsProducerMock>* file_logs_producer_mock;
+        StrictMock<AsyncLogQueueEngineMock>* async_log_queue_engine_mock;
+        EquinoxLoggerEngineImplTestable equinox_Logger_engine_impl;
     };
 
 }  // namespace equinox_logger_engine_impl_test
